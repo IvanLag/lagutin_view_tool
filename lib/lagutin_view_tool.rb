139b2cb8850e34c58fddec75b9cf0e5fa7126c51
Ivan Lagutin <ivan.lag95@gmail.com>
2023-09-08 11:00:16 +0400

    View tool initial commit

---
+++ lib/lagutin_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "lagutin_view_tool/version"
+require_relative "lagutin_view_tool/renderer"
+
+module LagutinViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
